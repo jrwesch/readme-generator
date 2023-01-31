f0c452298d5249a812ef7e3692bab2de8f41c5b2
jrwesch <jrwesch@gmail.com>
2023-01-31 10:12:24 -0500

    finished the license info, still need to get table of contents to generate in markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,7 +18,7 @@ function renderLicenseBadge(license) {
   } else if (license===licenseArr[6]){
     return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
   } else {
-    return ""
+    return "None"
   }
 
 }
@@ -40,15 +40,32 @@ function renderLicenseLink(license) {
     return `[${licenseArr[5]}](https://opensource.org/licenses/Apache-2.0)`
   } else if (license===licenseArr[6]){
     return `[${licenseArr[6]}](https://www.boost.org/LICENSE_1_0.txt)`
-  } else if (license===licenseArr[7]){
   } else {
-    return ""
+    return "None"
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license===licenseArr[0]){
+    return `Read more about ${licenseArr[0]} here:`
+  } else if (license===licenseArr[1]){
+    return `Read more about ${licenseArr[1]} here:`
+  } else if (license===licenseArr[2]){
+    return `Read more about ${licenseArr[2]} here:`
+  } else if (license===licenseArr[3]){
+    return `Read more about ${licenseArr[3]} here:`
+  } else if (license===licenseArr[4]){
+    return `Read more about ${licenseArr[4]} here:`
+  } else if (license===licenseArr[5]){
+    return `Read more about ${licenseArr[5]} here:`
+  } else if (license===licenseArr[6]){
+    return `Read more about ${licenseArr[6]} here:`
+  } else {
+    return "None"
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
